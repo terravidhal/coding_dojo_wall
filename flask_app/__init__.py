d6c3426fcbd99e1dd0d3dcf10a9f4dd9eb7f1ef3
terravidhal <infiniterraetincelle@gmail.com>
2023-09-21 02:45:31 +0100

    delete mp db & key

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask 
 app = Flask(__name__)  
-app.secret_key = "38377fb66f46c8f0ef86d2d207bb44204817543e22a5327a6c40dab94b8a9380"    # python -c 'import secrets; print(secrets.token_hex())'
+app.secret_key = ""    # python -c 'import secrets; print(secrets.token_hex())'
                        
 
 
